e8225530ca93cbf5fe0715f1745cdabb9c74cb58
nolgd <39465135+nolgd@users.noreply.github.com>
2019-04-06 18:27:11 -0400

    meow
    uwu

--- HDF/HDF/Game.cs
+++ HDF/HDF/Game.cs
@@ -8,5 +8,16 @@ namespace HDF
 {
     class Game
     {
+        public static int LEFT_EDGE = 10;
+        public static int TOP_EDGE = 10;
+        public static int RIGHT_EDGE = 790;
+        public static int BOTTOM_EDGE = 450;
+
+        private Random random;
+
+        private Razor razor;
+        private List<IDrawable> drawables;
+
+
     }
 }
---
+++ HDF/HDF/interfaces.cs
@@ -0,0 +1,28 @@
+﻿
+using Microsoft.Graphics.Canvas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Windows.Gaming.Input;
+using Windows.UI;
+
+namespace HDF
+{
+    
+        public interface IDrawable
+        {
+            void Draw(CanvasDrawingSession canvas);
+        }
+
+        public interface ICollidable
+        {
+            bool CollidesLeftEdge(int x, int y);
+            bool ColllidesRightEdge(int x, int y);
+            bool CollidesTopEdge(int x, int y);
+            bool CoolidesBottomEdge(int x, int y);
+        }
+
+        public interface IDestroyable : ICollidable
+        { }
+    
+}
