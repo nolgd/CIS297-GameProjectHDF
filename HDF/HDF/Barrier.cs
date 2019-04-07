7da739100ca0f9c0cc0d70538b66beb01d101c38
nolgd <39465135+nolgd@users.noreply.github.com>
2019-04-07 18:28:45 -0400

    maybe stuff was changed in this commit
    notsure

--- HDF/HDF/Barrier.cs
+++ HDF/HDF/Barrier.cs
@@ -6,22 +6,36 @@ using System.Threading.Tasks;
 using Microsoft.Graphics.Canvas;
 using Windows.UI;
 using Windows.UI.Xaml.Media;
+using System.Drawing;
 
 namespace HDF
 {
+
     class Barrier : IDrawable, ICollidable
     {
+        public struct rectD
+        {
+            public double x;
+            public double y;
+            public int width;
+            public int height;
+        };
         public RotateTransform rotate;
-
+        
         public int X { get; set; }
         public int Y { get; set; }
         public int Width { get; set; }
         public int Height { get; set; }
-        public Color Color { get; set; }
+        public Windows.UI.Color Color { get; set; }
         public bool rotateClockwise { get; set; }
-
+        public Rectangle rect { get; set; }
+        public Graphics g { get; set; }
+        private int startDraw;
+        private List<rectD> circle;
         public Barrier()
         {
+            startDraw = 330;
+            circle = new List<rectD>();
             rotate = new RotateTransform();
             X = 500;
             Y = 200;
@@ -32,6 +46,17 @@ namespace HDF
             rotate.CenterX = 500;
             rotate.CenterY = 500;
             rotate.Angle = 0;
+            rect = new Rectangle(500, 500, 20, 20);
+
+            for(int i = 0; i < 360; i++)
+            {
+                rectD temp1 = new rectD();
+                temp1.x = (500 + 300 * System.Math.Cos(i));
+                temp1.y = (500 + 300 * System.Math.Sin(i));
+                temp1.width = 1;
+                temp1.height = 1;
+                circle.Add(temp1);
+            }
         }
 
         public bool CollidesLeftEdge(int x, int y)
@@ -56,7 +81,21 @@ namespace HDF
 
         public void Draw(CanvasDrawingSession canvas)
         {
-            canvas.DrawRectangle(X, Y, Width, Height, Color, 3);
+            
+            int sos = startDraw;
+            int count = 0;
+            while (count < 20)
+            {
+                if (sos + count >= 360)
+                {
+                    sos = 0 - count;
+                }
+                rectD temp1 = circle[sos + count];
+                canvas.DrawRectangle((float)temp1.x,(float)temp1.y,temp1.width,temp1.height,Color,1);
+                count++;
+            }
+            //canvas.DrawRectangle(X, Y, Width, Height, Color, 3);
+            //canvas.DrawRectangle(rect);
         }
 
         public void Update()
@@ -70,11 +109,15 @@ namespace HDF
 
             if (rotateClockwise)
             {
-               rotate.Angle += 1;
+               startDraw += 1;
+                if (startDraw > 359)
+                    startDraw = 0;
             }
             else if (!rotateClockwise)
             {
-                rotate.Angle -= 1;
+                startDraw -= 1;
+                if (startDraw < 0)
+                    startDraw = 359;
             }
         
         }
